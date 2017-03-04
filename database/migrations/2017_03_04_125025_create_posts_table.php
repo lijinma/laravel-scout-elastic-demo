dcf66b571617e87f2586554dca5ce3449de316df
Jinma li <lijinma@126.com>
2017-03-04 21:02:33 +0800

    - init demo

---
+++ database/migrations/2017_03_04_125025_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('url');
+            $table->string('author', 64)->nullable()->default(null);
+            $table->text('title');
+            $table->longText('content');
+            $table->dateTime('post_date')->nullable()->default(null);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
